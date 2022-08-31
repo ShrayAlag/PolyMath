eaba4a4449d90133bb862ca8807025e575708870
Siddharth <siddrrsh@gmail.com>
2022-08-31 16:50:38 -0700

    V1 Finalized.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, flash
-from flask import request
+from flask import Flask, render_template, request, flash, redirect, url_for
 import random
 import requests
 from bs4 import BeautifulSoup
@@ -13,7 +12,7 @@ app = Flask(__name__)
 app.secret_key = "Polymath"
 
 
-
+classifier = None
 sub_topics = []
 def remove_tags(text):
   clean = re.compile('<.*?>')
@@ -33,7 +32,6 @@ def extract_article(topic):
 
   content = remove_tags(text)
 
-  """
   files = re.findall('href="/wiki/\w*"', text)
   lst = sorted(x for x in (files))
 
@@ -42,32 +40,9 @@ def extract_article(topic):
     lst[i] = lst[i][6:]
   related_articles = lst
 
-  """
-  res = (topic, content)
+  res = (topic, content, related_articles)
   return res
 
-
-def extract_init_topics(topic):
-  url = 'https://en.wikipedia.org/wiki/' + topic
-  page = requests.get(url)
-
-  # scrape webpage
-  soup = BeautifulSoup(page.content, 'html.parser')
-  list(soup.children)
-  text = str(soup.find_all('p'))
-
-  content = remove_tags(text)
-  #flash(content)
-
-  files = re.findall('href="/wiki/\w*"', text)
-  lst = sorted(x for x in (files))
-
-  for i in range(len(lst)):
-    lst[i] = (lst[i][6:])[:-1]
-    lst[i] = lst[i][6:]
-
-  related_articles = lst
-  return related_articles
 
 def get_subject_list():
   subjects = ['Cooking', 'Economics', 'biology', 'chemistry', 'physics', 'sports', 'tennis', 'basketball', 'countries', 'football', 'math', 'geography', 'literature', 'music', 'nature', 'movies', 'transportation', 'politics', 'philosophy', 'aeronautics', 'art', 'history', 'computer science', 'engineering', 'dance', 'neuroscience', 'law']
@@ -99,16 +74,13 @@ def check_article_exists(topic):
 
 # This is the main one: 
 def topic_information(topic_chosen):
-  orig_topic = topic_chosen
   topic_chosen = "".join(topic_chosen.rstrip().lstrip())
 
   #topic_chosen = topic_chosen.lower()
   output = ''
   if topic_chosen == 'r':
     topic_chosen = random.choice(get_subject_list())
-    output = 'You selected: ' + topic_chosen
-  else:
-    output = 'You selected: ' + orig_topic
+  output = 'You selected: ' + topic_chosen
   flash(output)
 
   if not check_article_exists(topic_chosen):
@@ -116,10 +88,9 @@ def topic_information(topic_chosen):
     return ''
   
   ### SUMMARY CODE
-  classifier = pipeline("summarization")
   str_result = ""
   tup = extract_article(topic_chosen)
-  summarized = classifier((tup[1])[:2048])
+  summarized = classifier((tup[1])[:850])
   cleaned_topic = topic_chosen.replace('_', ' ')
   str_result += "Here's some info about: " + cleaned_topic + " ... "
   str_result += fix_periods_issue((summarized[0])['summary_text'])
@@ -128,7 +99,7 @@ def topic_information(topic_chosen):
   cleaned_sample = []
   sub_topics_str = " "
 
-  related_topics = extract_init_topics(topic_chosen)
+  related_topics = tup[2]
   if len(related_topics) > 6:
     related_topics = random.sample(related_topics, 6)
 
@@ -163,48 +134,6 @@ def topic_information(topic_chosen):
 ## To-Dos: continue styling + use fonts, add a way to check that the article does have a wiki. Add a learn more about this 
 
 
-def check_topic_in_wiki():
-  return NotImplementedError()
-
-
-
-
-
-def info_abt_sub_topic():
-  sub_topic_chosen = request.args.get("sub_topic", "") 
-  if sub_topic_chosen: #if not none
-    return sub_topic_information(sub_topic_chosen)
-  else:
-    return ""
-
-def sub_topic_information(topic):
-  classifier = pipeline("summarization")
-  str_result = ""
-  tup = extract_article(topic)
-  summarized = classifier((tup[1])[:2048])
-  cleaned_topic = topic.replace('_', ' ')
-  str_result += "Here's some info about:" + cleaned_topic
-  str_result += (summarized[0])['summary_text']
-  str_result += " Here are some articles related to" + topic
-
-  sub_sample = random.sample(tup[2], 6)
-  cleaned_sample = []
-  for i in range(len(sub_sample)):
-    new = ''
-    for ch in sub_sample[i]:
-      if ch == '_':
-        new += ' '
-      else:
-        new += ch
-    cleaned_sample.append(new)
-  counter = 1
-  for item in cleaned_sample:
-    str_result += {counter} + " " + {item}
-    counter += 1
-  str_result += " "
-  str_result += "Enter above to explore any subtopic"
-  str_result += "Thanks for using Polymath!"
-  return str_result
 
 def print_topics():
   subjects = get_subject_list()
@@ -213,21 +142,12 @@ def print_topics():
     upper_case_topic = topic[0].upper() + topic[1:]
     str_build_up += upper_case_topic + ", "
   return str_build_up[:len(str_build_up) - 2]
-
-def info_abt_topic():
-  topic_chosen = request.args.get("topic", "") 
-  sub_topic_chosen = request.args.get("sub_topic", "") 
-  if topic_chosen or sub_topic_chosen: #if not none
-    return topic_information(topic_chosen) + """<form action="" method="get">
-            Which topic would you like to keep exploring (enter topic name):  <input type="text" name="sub_topic">
-            <input type="submit" value="Get Summary">
-        </form>"""
-  else:
-    return ""
     
 @app.route("/home")
 def index():
     flash(print_topics())
+    global classifier
+    classifier = pipeline("summarization")
     return render_template('home.html')
 
 @app.route("/information", methods=["POST", "GET"])
@@ -236,130 +156,11 @@ def information():
   flash(topic_information(topic))
   return render_template('search.html')
 
+@app.route("/")
+def redirectToHome():
+  return redirect(url_for('index'))
+  
 # topic_information: should give summarization and sub_topics
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8090, debug=True)
-
-
-# def client():
-#   classifier = pipeline("summarization")
-#   print("Hi, welcome to Polymath!")
-#   # option for random info (any subject) or explore particular subject
-
-#   subjects = ['cooking', 'economics', 'biology', 'chemistry', 'physics', 'sports', 'tennis', 'basketball', 'countries', 'football', 'math', 'geography', 'literature', 'music', 'nature', 'movies', 'transportation', 'politics', 'philosophy', 'aeronautics', 'art', 'history', 'computer_science', 'engineering', 'dance', 'neuroscience', 'law']
-#   print("Polymath is a curiosity network that lets you learn about new ideas and topics by exploring the web.")
-#   print()
-#   print("Here are some common topics!: ", subjects)
-#   init_topic = input("Pick one to get started or type in 'r' to explore a random one: ")
-#   if init_topic == 'r':
-#     init_topic = random.choice(subjects)
-#   related_topics = random.sample(extract_init_topics(init_topic), 6)
-
-#   cleaned_sample = []
-#   for i in range(len(related_topics)):
-#     new = ''
-#     for ch in related_topics[i]:
-#       if ch == '_':
-#         new += ' '
-#       else:
-#         new += ch
-#     cleaned_sample.append(new)
-
-#   print("Here are some related topics to start exploring for the category of: ", init_topic)
-#   counter = 1
-#   for item in cleaned_sample:
-#     print(counter, item)
-#     counter += 1
-#   print()
-#   user_next = input("Which topic would you like to keep exploring (enter a number or enter nothing to quit): ")
-#   topic = ''
-#   if user_next != "":
-#     topic = related_topics[int(user_next)-1]
-#   else:
-#     quit()
-
-#   while True:
-#     tup = extract_article(topic)
-#     summarized = classifier((tup[1])[:1024])
-#     cleaned_topic = topic.replace('_', ' ')
-#     print("Here's some info about: ", cleaned_topic)
-#     print((summarized[0])['summary_text'])
-#     print("Here are some related articles: ")
-#     sub_sample = random.sample(tup[2], 6)
-#     cleaned_sample = []
-#     for i in range(len(sub_sample)):
-#       new = ''
-#       for ch in sub_sample[i]:
-#         if ch == '_':
-#           new += ' '
-#         else:
-#           new += ch
-#       cleaned_sample.append(new)
-#     counter = 1
-#     for item in cleaned_sample:
-#       print(counter, item)
-#       counter += 1
-#     print()
-#     user_next = input("Which topic would you like to keep exploring (enter a number or enter nothing to quit): ")
-#     if user_next != "":
-#       topic = sub_sample[int(user_next)-1]
-#     else:
-#       break
-
-#   print("Thanks for using Polymath!")
-
-# def print_topics():
-#   subjects = get_subject_list()
-#   str_build_up = ""
-#   for topic in subjects:
-#     upper_case_topic = topic[0].upper() + topic[1:]
-#     str_build_up += upper_case_topic + ", "
-#   return str_build_up[:len(str_build_up) - 2]
-
-# def info_abt_topic():
-#   topic_chosen = request.args.get("topic", "") 
-#   if topic_chosen: #if not none
-#     return topic_information(topic_chosen) + """<form action="" method="get">
-#             Which topic would you like to keep exploring (enter topic name):  <input type="text" name="sub_topic">
-#             <input type="submit" value="Get Summary">
-#         </form>"""
-#   else:
-#     return ""
-
-
-# def fahrenheit_from(celsius):
-#     """Convert Celsius to Fahrenheit degrees."""
-#     try:
-#         fahrenheit = float(celsius) * 9 / 5 + 32
-#         fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-#         return str(fahrenheit)
-#     except ValueError:
-#         return "invalid input"
-
-
-# @app.route("/")
-# def index():
-#     celsius = request.args.get("celsius", "")
-#     if celsius:
-#         fahrenheit = fahrenheit_from(celsius)
-#     else:
-#         fahrenheit = ""
-#     return (
-#         """<form action="" method="get">
-#                 Celsius temperature: <input type="text" name="celsius">
-#                 <input type="submit" value="Convert to Fahrenheit">
-#             </form>"""
-#         + "Fahrenheit: "
-#         + fahrenheit
-#     )
-
-
-# if __name__ == "__main__":
-#     app.run(host="127.0.0.1", port=8080, debug=True)
-
-#####################################################################
-
-# from flask import Flask
-
-# app = Flask(__name__)
